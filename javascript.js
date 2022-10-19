757b223ae9c6968e4b08d0163c1c9dde09f9cfae
ChonkyVacation <chris.mclaren1@hotmail.com>
2022-10-19 23:13:21 +1100

    Fixed getComputerChoice function to change with each round

--- javascript.js
+++ javascript.js
@@ -1,26 +1,12 @@
-function getRandomNumber(min, max){
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-let computerChoice = (getRandomNumber(1, 3));
-
-function getComputerChoice() {
-
-if(computerChoice === 1) {
-    return("rock"); }
-else if(computerChoice === 2) {
-    return("paper"); }
-else if (computerChoice === 3) {
-    return("scissors"); }
-}
-
-
 let compScore = 0
 let playerScore = 0
 
+const playerScissors = document.querySelector('.container').querySelector('.player').querySelector('.playerChoices').querySelector('.playerScissors');
+const playerPaper = document.querySelector('.container').querySelector('.player').querySelector('.playerChoices').querySelector('.playerPaper');
+const playerRock = document.querySelector('.container').querySelector('.player').querySelector('.playerChoices').querySelector('.playerRock');
+
 function playRound(playerSelection, computerSelection) {
+    const outcomeDiv = document.querySelector('.outcome');
     if (playerSelection === computerSelection) {
         const p = document.createElement('p')
         p.innerText = `you tied! you both picked ${playerSelection}`
@@ -57,16 +43,10 @@ function playRound(playerSelection, computerSelection) {
         outcomeDiv.appendChild(p) }
     }
 
-const outcomeDiv = document.querySelector('.outcome');
-const playerScissors = document.querySelector('.container').querySelector('.player').querySelector('.playerChoices').querySelector('.playerScissors');
-const playerPaper = document.querySelector('.container').querySelector('.player').querySelector('.playerChoices').querySelector('.playerPaper');
-const playerRock = document.querySelector('.container').querySelector('.player').querySelector('.playerChoices').querySelector('.playerRock');
-
 playerScissors.addEventListener('click', () => {
-    const playerSelection = "scissors";
     const computerSelection = getComputerChoice()
+    const playerSelection = "scissors";
     playRound(playerSelection, computerSelection)
-    console.log(computerSelection);
     });
 
 playerPaper.addEventListener('click', () => {
@@ -95,3 +75,18 @@ function game() {
         alert ("You lost to the computer!"); }
 }
 
+function getRandomNumber(min, max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getComputerChoice() {
+let computerChoice = (getRandomNumber(1, 3));
+if(computerChoice === 1) {
+    return("rock"); }
+else if(computerChoice === 2) {
+    return("paper"); }
+else if (computerChoice === 3) {
+    return("scissors"); }
+}
